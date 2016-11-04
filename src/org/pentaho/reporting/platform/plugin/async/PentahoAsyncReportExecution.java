fee83e637fa49eb918a829566cf4ee6c8b1e37f5
nbaker <nbaker@pentaho.com>
2016-11-04 11:37:35 -0400

    Prototype

--- src/org/pentaho/reporting/platform/plugin/async/PentahoAsyncReportExecution.java
+++ src/org/pentaho/reporting/platform/plugin/async/PentahoAsyncReportExecution.java
@@ -25,6 +25,7 @@ import org.apache.commons.logging.LogFactory;
 import org.pentaho.platform.api.engine.IPentahoSession;
 import org.pentaho.platform.engine.core.audit.MessageTypes;
 import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
+import org.pentaho.platform.engine.security.SecurityHelper;
 import org.pentaho.reporting.engine.classic.core.MasterReport;
 import org.pentaho.reporting.libraries.resourceloader.ResourceException;
 import org.pentaho.reporting.platform.plugin.AuditWrapper;
@@ -79,62 +80,61 @@ public class PentahoAsyncReportExecution extends AbstractAsyncReportExecution<IA
    * @throws Exception
    */
   @Override public IFixedSizeStreamingContent call() throws Exception {
-    final AsyncReportStatusListener listener = getListener();
-    if ( listener == null ) {
-      throw new NullPointerException( "No listener for async report execution: " + url );
-    }
+    return SecurityHelper.getInstance().runAsUser( safeSession.getName(), () -> {
+      final AsyncReportStatusListener listener = getListener();
+      if ( listener == null ) {
+        throw new NullPointerException( "No listener for async report execution: " + url );
+      }
 
-    try {
-      listener.setStatus( AsyncExecutionStatus.WORKING );
+      try {
+        listener.setStatus( AsyncExecutionStatus.WORKING );
 
-      PentahoSessionHolder.setSession( safeSession );
+        ReportListenerThreadHolder.setListener( listener );
+        ReportListenerThreadHolder.setRequestId( auditId );
 
-      ReportListenerThreadHolder.setListener( listener );
-      ReportListenerThreadHolder.setRequestId( auditId );
-
-      final long start = System.currentTimeMillis();
-
-      getAudit().audit( safeSession.getId(), safeSession.getName(), url, getClass().getName(), getClass().getName(),
-        MessageTypes.INSTANCE_START, auditId, "", 0, null );
-
-      if ( reportComponent.execute() ) {
-
-        final long end = System.currentTimeMillis();
+        final long start = System.currentTimeMillis();
 
         getAudit().audit( safeSession.getId(), safeSession.getName(), url, getClass().getName(), getClass().getName(),
-          MessageTypes.INSTANCE_END, auditId, "", ( (float) ( end - start ) / 1000 ), null );
+          MessageTypes.INSTANCE_START, auditId, "", 0, null );
 
-        final IFixedSizeStreamingContent stagingContent = handler.getStagingContent();
+        if ( reportComponent.execute() ) {
 
-        listener.setStatus( AsyncExecutionStatus.FINISHED );
+          final long end = System.currentTimeMillis();
 
-        return stagingContent;
+          getAudit().audit( safeSession.getId(), safeSession.getName(), url, getClass().getName(), getClass().getName(),
+            MessageTypes.INSTANCE_END, auditId, "", ( (float) ( end - start ) / 1000 ), null );
+
+          final IFixedSizeStreamingContent stagingContent = handler.getStagingContent();
+
+          listener.setStatus( AsyncExecutionStatus.FINISHED );
+
+          return stagingContent;
+        }
+
+        // in case execute just returns false without an exception.
+        fail();
+        return NULL;
+      } catch ( final Throwable ee ) {
+        // it is bad practice to catch throwable.
+        // but we has to to set proper execution status in any case.
+        // Example: NoSuchMethodError (instance of Error) in case of usage of
+        // uncompilable jar versions.
+        // We have to avoid to hang on working status.
+        log.error( "fail to execute report in async mode: " + ee );
+
+        if ( ee.getMessage() != null ) {
+          listener.setErrorMessage( ee.getMessage() );
+        }
+        fail();
+        return NULL;
+      } finally {
+        // in case report processor not going to close it
+        OutputStream out = handler.getStagingOutputStream();
+        IOUtils.closeQuietly( out );
+
+        ReportListenerThreadHolder.clear();
       }
-
-      // in case execute just returns false without an exception.
-      fail();
-      return NULL;
-    } catch ( final Throwable ee ) {
-      // it is bad practice to catch throwable.
-      // but we has to to set proper execution status in any case.
-      // Example: NoSuchMethodError (instance of Error) in case of usage of
-      // uncompilable jar versions.
-      // We have to avoid to hang on working status.
-      log.error( "fail to execute report in async mode: " + ee );
-
-      if ( ee.getMessage() != null ) {
-        listener.setErrorMessage( ee.getMessage() );
-      }
-      fail();
-      return NULL;
-    } finally {
-      // in case report processor not going to close it
-      OutputStream out = handler.getStagingOutputStream();
-      IOUtils.closeQuietly( out );
-
-      ReportListenerThreadHolder.clear();
-      PentahoSessionHolder.removeSession();
-    }
+    } );
   }
 
   @Override public String toString() {
